f68cd18c7fb7667e645b247aff6c220e3d130260
Michael Ford <mford@sfu.ca>
2022-08-29 17:26:55 -0400

    Add install features

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,43 +1,57 @@
 import os
 
-init_git = '{{ cookiecutter.init_git }}'.split(' -')[0] == 'Yes'
-if init_git:
-    print('\nInitializing git...')
-    os.system('git init')
-
 print("""
 
 -------------------------
 Success! Project created!
 -------------------------
-Before you start working, dont forget to head to your project and:
+Configuring desired features...
+""")
+
+conda_install = '{{ cookiecutter.conda_install }}'.split(' -')[0] == 'Yes'
+if conda_install:
+    print(f"Installing default conda environment...")
+    os.system("conda env create -f {{ cookiecutter.conda_environment_file }} --experimental-solver libmamba")
+    os.system('eval "$(conda shell.bash hook)" && conda activate {{ cookiecutter.conda_environment_name}}')
+else:
+    print("""Not installing default conda environment - after modification install the environment:
 1.  Created your conda environment (modify as needed) - `conda env create -f {{ cookiecutter.conda_environment_file }}`
 2.  Activate your conda environment - `conda activate {{ cookiecutter.conda_environment_name}}`""")
-info_counter = 3
+
+
+init_git = '{{ cookiecutter.init_git }}'.split(' -')[0] == 'Yes'
+if init_git:
+    print('\nInitializing git...')
+    os.system('git init')
+
 
 copy_git_hooks = '{{ cookiecutter.conda_git_hooks }}'.split(' -')[0] == 'Yes'
 if not copy_git_hooks:
     os.system('rm .git/hooks/pre-commit .git/hooks/post-merge')
 else:
-    if not init_git:
-        print(f"{info_counter}.  Initialize the project as a git repo - `git init`")
-        info_counter +=1
-        
-    print(f"{info_counter}.  Set the conda git hooks as executable - `chmod u+x .git/hooks/pre-commit .git/hooks/post-merge`")
-    info_counter +=1
+    print(f"\nSetting the conda git hooks as executable - `chmod u+x .git/hooks/pre-commit .git/hooks/post-merge`...")
+    os.system('chmod u+x .git/hooks/pre-commit .git/hooks/post-merge')
 
-    print(f"{info_counter}.  Add and commit {{ cookiecutter.conda_environment_file }} - `git add {{ cookiecutter.conda_environment_file }} && git`")
-    info_counter +=1
+dvc_init = '{{ cookiecutter.dvc_init }}'.split(' -')[0] == 'Yes'
+if dvc_init:
+    print(f"\nInitializing DVC...")
+    os.system('dvc init')
 
-print(f"{info_counter}.  Run `python setup.py develop` to install src/ as a python module")
-info_counter +=1
+dvc_path = '{{ cookiecutter.dvc_cache_path }}'
+if '[OPTIONAL]' not in dvc_path:
+    if os.path.isdir(dvc_path):
+        os.system(f"dvc cache {dvc_path}")
+    else:
+        print(f"ERROR: Cannot change DVC cache path: {dvc_path} doesn't exist")
 
-print('subrepo_name')
-print('{{ cookiecutter.subrepo_name }}')
+print(f"\nAdding {{ cookiecutter.conda_environment_file }} - `git add {{ cookiecutter.conda_environment_file }}`")
+os.system('git add {{ cookiecutter.conda_environment_file }}')
+
+print(f"Running `python setup.py develop` to install src/ as a python module...")
+
 intialize_subrepo = '{{ cookiecutter.subrepo_name }}' if '[OPTIONAL]' not in '{{ cookiecutter.subrepo_name }}' else None
 if intialize_subrepo:
-    print(f"{info_counter}.  Install git-subrepo and setup (see README.md)")
-    info_counter +=1
-    
+    print(f"Installing git-subrepo and setup (see README.md)...")
+    print("NOT IMPLEMENTED")    
 print('\n')
 
