54a6fc3ee9ca5f4ff397828c5be1a93bd5806d7f
Michael Ford <mford@sfu.ca>
2023-04-25 00:00:09 -0400

    Bugfix in hooks/post_gen_project.py

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,4 +1,9 @@
 import os
+export_flag = True
+try:
+    from .{{ cookiecutter.conda_environment_name }}_env_export import main as export_conda_env
+except ImportError:
+    export_flag = False
 
 print("""
 
@@ -8,11 +13,15 @@ Success! Project created!
 Configuring desired features...
 """)
 
+
 conda_install = '{{ cookiecutter.conda_install }}'.split(' -')[0] == 'Yes'
 if conda_install:
     print(f"Installing default conda environment...")
     os.system("conda env create -f {{ cookiecutter.conda_environment_file }} --experimental-solver libmamba")
     os.system('eval "$(conda shell.bash hook)" && conda activate {{ cookiecutter.conda_environment_name}}')
+    # Export version numbers if option was selected
+    if not "Don't" in "{{ cookiecutter.conda_env_tracking_version_numbers }}" and export_flag:
+        export_conda_env("{{ cookiecutter.conda_environment_file }}")
 else:
     print("""Not installing default conda environment - after modification install the environment:
 1.  Created your conda environment (modify as needed) - `conda env create -f {{ cookiecutter.conda_environment_file }}`
@@ -27,10 +36,10 @@ if init_git:
 
 copy_git_hooks = '{{ cookiecutter.conda_git_hooks }}'.split(' -')[0] == 'Yes'
 if not copy_git_hooks:
-    os.system('rm .git/hooks/pre-commit .git/hooks/post-merge')
+    os.system('rm -r git-hooks/')
 else:
-    print(f"\nSetting the conda git hooks as executable - `chmod u+x .git/hooks/pre-commit .git/hooks/post-merge`...")
-    os.system('chmod u+x .git/hooks/pre-commit .git/hooks/post-merge')
+    print(f"\nSetting git-hooks/* as executable and symlinking to .git/hooks/...")
+    os.system('chmod u+x git-hooks/* && cd .git/hooks && ln -s ../../git-hooks/*')
 
 dvc_init = '{{ cookiecutter.dvc_init }}'.split(' -')[0] == 'Yes'
 if dvc_init:
@@ -40,7 +49,7 @@ if dvc_init:
 dvc_path = '{{ cookiecutter.dvc_cache_path }}'
 if '[OPTIONAL]' not in dvc_path:
     if os.path.isdir(dvc_path):
-        os.system(f"dvc cache {dvc_path}")
+        os.system(f"mkdir -p {dvc_path} && dvc cache dir {dvc_path}")
     else:
         print(f"ERROR: Cannot change DVC cache path: {dvc_path} doesn't exist")
 
